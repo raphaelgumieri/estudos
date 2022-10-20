262b3241ecb39c42e02387069f7f9689f92b1862
raphaelgumieri <raphaelgumieri@hotmail.com>
2022-10-20 15:54:22 -0300

    despesas pessoais: alinhando textos pelo crossAxis e estilizando texto

--- flutter/aprenda_flutter_e_desenvolva_apps_para_android_e_ios_2022/m02_construindo_app_real_despesas_pessoais/despesas_pessoais/lib/main.dart
+++ flutter/aprenda_flutter_e_desenvolva_apps_para_android_e_ios_2022/m02_construindo_app_real_despesas_pessoais/despesas_pessoais/lib/main.dart
@@ -77,9 +77,21 @@ class MyHomePage extends StatelessWidget {
                           ),
                         ),
                         Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: <Widget>[
-                            Text(tr.title),
-                            Text(tr.date.toString()),
+                            Text(
+                              tr.title,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                            Text(
+                              tr.date.toString(),
+                              style: TextStyle(
+                                color: Colors.grey,
+                              ),
+                            ),
                           ],
                         ),
                       ],
