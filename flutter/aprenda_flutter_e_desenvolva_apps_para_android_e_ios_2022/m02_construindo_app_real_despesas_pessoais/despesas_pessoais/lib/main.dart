ffcfd97d6f9258aea5c08d53c62a0d6af577152f
raphaelgumieri <raphaelgumieri@hotmail.com>
2022-10-20 16:47:30 -0300

    despesas pessoais: usando textfield

--- flutter/aprenda_flutter_e_desenvolva_apps_para_android_e_ios_2022/m02_construindo_app_real_despesas_pessoais/despesas_pessoais/lib/main.dart
+++ flutter/aprenda_flutter_e_desenvolva_apps_para_android_e_ios_2022/m02_construindo_app_real_despesas_pessoais/despesas_pessoais/lib/main.dart
@@ -40,7 +40,6 @@ class MyHomePage extends StatelessWidget {
         title: Text('Despesas Pessoais'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           SizedBox(
@@ -100,6 +99,38 @@ class MyHomePage extends StatelessWidget {
                   ),
                 )
                 .toList(),
+          ),
+          Card(
+            elevation: 5,
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Título',
+                    ),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Valor (R\$)',
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(
+                        onPressed: () {},
+                        style: TextButton.styleFrom(
+                          foregroundColor: Colors.purple,
+                        ),
+                        child: Text('Nova Transação'),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
       ),
